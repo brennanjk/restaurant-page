50c2cfe5f93e951f2c617b9b57c1fd2171f8fb80
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-07-26 10:28:23 -0700

    Add header title and header menu/list content to be created dynamically in page-load module

--- src/page-load.js
+++ src/page-load.js
@@ -1,7 +1,41 @@
 
 function createHeader() {
+    //create main header div
     const header = document.createElement('div');
     header.classList.add('header');
+
+    //creater header title for Restaurant name
+    const headerTitle = document.createElement('div');
+    headerTitle.classList.add('rest-name');
+    headerTitle.textContent="Moe's Burger Joint";
+
+    //create restaurant header list/menu
+    const headerList = document.createElement('ul');
+    headerList.classList.add('header-menu');
+
+    //create header list items and append them to the headerList
+    const homeItem = document.createElement('li');
+    homeItem.classList.add('home');
+    homeItem.classList.add('header-item');
+    homeItem.textContent="Home";
+    
+    const menuItem = document.createElement('li');
+    menuItem.classList.add('menu');
+    menuItem.classList.add('header-item');
+    menuItem.textContent = "Menu";
+
+    const contactItem = document.createElement('li');
+    contactItem.classList.add('contact');
+    contactItem.classList.add('header-item');
+    contactItem.textContent = "Contact";
+
+    headerList.appendChild(homeItem);
+    headerList.appendChild(menuItem);
+    headerList.appendChild(contactItem);
+
+    //append header content to header div
+    header.appendChild(headerTitle);
+    header.appendChild(headerList);
 
     return header;
 }
