c1712839cc3163d20e517ee953f0e2b62d6a1b87
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-07-27 16:36:17 -0700

    Add menu item picture and name. Fix issue with CSS targeting that was affecting menu item
    CSS was targeting '.container > *' to set a fixed width on the home page image, but this was affecting the menu item when it loaded. Changed property target '.container > img' to avoid this

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,6 @@
 function clearContent() {
     const mainContainer = document.querySelector('.container');
-    
+
     //Clear main content section
     while (mainContainer.firstChild) {
         mainContainer.removeChild(mainContainer.firstChild);
@@ -20,10 +20,15 @@ function createMenu() {
     menuItem1.classList.add('menu-item');
     const description1 = document.createElement('div');
     description1.classList.add('item-details');
-    description1.textContent = 'Test item';
+    description1.textContent = 'Classic Cheeseburger';
+    const image1 = document.createElement('img');
+    image1.classList.add('menu-img');
+    image1.src = './images/Classic Cheeseburger.jpg';
+    image1.alt = 'Classic Cheesburger. Photo Credit Valeria Boltneva';
   
     //append objects 
     menuItem1.appendChild(description1);
+    menuItem1.appendChild(image1);
     menu.appendChild(menuItem1);
     mainContainer.appendChild(menu);
 }
