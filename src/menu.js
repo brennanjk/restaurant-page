de3ef6573fdea908cd547dd5024035adb705637f
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-07-27 09:55:39 -0700

    Add test content to menu module to test adding content after container is cleared

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,36 @@
 function clearContent() {
     const mainContainer = document.querySelector('.container');
     
+    //Clear main content section
     while (mainContainer.firstChild) {
         mainContainer.removeChild(mainContainer.firstChild);
     }
-    console.log('What the Heck');
     return {mainContainer, clearContent};
+}
+
+function createMenu() {
+    const mainContainer = document.querySelector('.container');
+
+    // create main menu container
+    const menu = document.createElement('div');
+    menu.classList.add('menu');
+
+    //create menu item
+    const menuItem1 = document.createElement('div');
+    menuItem1.classList.add('menu-item');
+    const description1 = document.createElement('div');
+    description1.classList.add('item-details');
+    description1.textContent = 'Test item';
+  
+    //append objects 
+    menuItem1.appendChild(description1);
+    menu.appendChild(menuItem1);
+    mainContainer.appendChild(menu);
 }
 
 function loadMenu() {
     clearContent();
+    createMenu();
 }
 
 export default loadMenu;
