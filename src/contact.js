3e79da7cbb295da703d9121090a1e7f8c3b60356
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-07-28 15:27:45 -0700

    Add contacts/location section

--- src/contact.js
+++ src/contact.js
@@ -5,11 +5,36 @@ function clearContent() {
     while (mainContainer.firstChild) {
         mainContainer.removeChild(mainContainer.firstChild);
     }
-    return {mainContainer, clearContent};
+    return {clearContent};
+}
+
+function createContacts() {
+    const mainContainer = document.querySelector('.container');
+
+    // Add contacts container
+    const contacts = document.createElement('div');
+    contacts.classList.add('contacts-container');
+
+    // Add location
+    const location = document.createElement('div');
+    const localTitle = document.createElement('h2');
+    localTitle.classList.add('contact-header')
+    localTitle.textContent = 'Location';
+    const address = document.createElement('p');
+    address.classList.add('address');
+    address.textContent = '985 Greystone Street\r\nBurbank, IL 60459'
+
+    // append elements to create location div structure
+    location.appendChild(localTitle);
+    location.appendChild(address);
+    contacts.appendChild(location);
+
+    mainContainer.appendChild(contacts);
 }
 
 function loadContact() {
     clearContent();
+    createContacts();
 }
 
 export default loadContact;
